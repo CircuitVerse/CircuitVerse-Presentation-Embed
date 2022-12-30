870826e337a933419b1bc8da58913ceaef31b73e
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2022-12-30 11:29:08 +0530

    remove unnecessary commented sample codes

--- Chrome Extension/src/bg/background.js
+++ Chrome Extension/src/bg/background.js
@@ -1,17 +1,4 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
-// example of using a message handler from the inject scripts
-
-// chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-//   chrome.pageAction.show(sender.tab.id);
-//   sendResponse();
-// });
-
+// On install, open the CircuitVerse Docs in a new tab
 chrome.runtime.onInstalled.addListener(function(object) {
   chrome.tabs.create({url: 'https://docs.circuitverse.org/#/chapter2/2cvforeducators?id=embed-circuitverse-simulation-in-google-slides'});
 });
